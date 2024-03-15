8799891c8e56edac582f0dabbb031db43540952f
nishantchy <nishantchy1234@gmail.com>
2024-03-16 01:34:25 +0545

    homepage ready

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,22 @@
+
+// links animations
+const buttonLoader = () => {
+    document.addEventListener("DOMContentLoaded", function() {
+        const links = document.querySelectorAll(".nav-link");
+    
+        links.forEach(function(link) {
+            link.addEventListener("click", function(event) {
+                links.forEach(function(link) {
+                    link.classList.remove("clicked");
+                });
+                event.currentTarget.classList.add("clicked");
+            });
+        });
+    });
+} 
+buttonLoader();
+
+// Search button animation
 const searchBtn = document.getElementById('search-btn');
 const searchOpen = document.getElementById('search-open');
 
@@ -5,4 +24,18 @@ const Search = () => {
     searchOpen.classList.toggle("active");
 }
 
-searchBtn.onclick = () => Search();
+searchBtn.onclick = (event) => {
+    event.stopPropagation(); 
+    Search();
+};
+
+document.addEventListener('click', (event) => {
+    const isClickInsideSearch = searchOpen.contains(event.target);
+    const isClickOnSearchBtn = searchBtn.contains(event.target);
+
+    if (!isClickInsideSearch && !isClickOnSearchBtn) {
+        searchOpen.classList.remove("active");
+    }
+});
+
+
